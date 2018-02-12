96d0a2c8b87ad46d2197465fbeb5a921cb8e4780
regnierlo <lois.regnier.93@gmail.com>
2018-02-12 12:13:16 +0100

    Début structure

--- Main.py
+++ Main.py
@@ -1,11 +1,2 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
- 
-from tkinter import * 
-
-fenetre = Tk()
-
-label = Label(fenetre, text="Hello World")
-label.pack()
-
-fenetre.mainloop()
+import VueJeu
+VueJeu
